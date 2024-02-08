5ba3ef05402a086a9606fc8603422b1f965272f0
Mark Feaver <mark.feaver@gmail.com>
2024-02-08 13:23:57 +1100

    Update dark mode icon support

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
-const { app, Tray, Menu, nativeImage } = require('electron/main');
+const { app, Tray, Menu, nativeImage, nativeTheme } = require('electron');
 const path = require('path');
 const fileSelect = require('./fileSelect.js');
 
-let tray;
+let tray = null;
 
 const iconPath = path.join(__dirname, 'assets', 'iconTemplate.png');
 const iconDarkPath = path.join(__dirname, 'assets', 'iconTemplateDark.png');
 
 app.whenReady().then(() => {
-  tray = new Tray(nativeImage.createFromPath(iconPath));
-  tray.setPressedImage(nativeImage.createFromPath(iconDarkPath));
+  if (app.dock) app.dock.hide();
+
+  tray = new Tray(nativeImage.createFromPath(getIcon()));
   
   const contextMenu = Menu.buildFromTemplate([
     { label: 'Select file', type: 'normal', click: () => { fileSelect.launchFileSelect() } },
     { type: 'separator' },
-    { label: 'Settings', type: 'normal', checked: true, click: () => { /* Add action for Item3 */ } },
+    { label: 'Settings', type: 'normal', checked: true, click: () => { /* Add action for Settings */ } },
     { label: 'Quit', type: 'normal', click: () => { quitApplication() } }
   ]);
   
@@ -28,4 +29,11 @@ function quitApplication() {
   } catch (err) {
     console.error("Error while quitting the application:", err);
   }
+}
+
+const getIcon = () => {
+  // TODO: Return an `.ico` for Windows
+  if  (process.platform === 'win32')  return iconPath;
+  if (nativeTheme.shouldUseDarkColors) return iconDarkPath;
+  return iconPath;
 }
